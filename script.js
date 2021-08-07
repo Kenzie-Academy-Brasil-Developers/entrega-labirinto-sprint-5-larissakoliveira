767f1e883d1fd7683071344347b41036fd3c0d68
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-08-07 10:13:18 -0300

    feat:

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const mainLabirinto = document.getElementById('labirinto')
 const button = document.getElementById('restart')
 const win = document.getElementById('victory')
 
+
 let map = [
     "WWWWWWWWWWWWWWWWWWWWW",
     "W   W     W     W W W",
@@ -41,16 +42,16 @@ let createLabyrinth = ()=>{
             
             if(map[i][j] === "S"){
                 item = document.createElement('img')
-                item.src = 'patin'
+                item.src = 'patin.jpeg'
                 createSmallDiv.appendChild(item)
             }
             if(map[i][j] === "W"){
-                createSmallDiv.style.background = 'green'
+                createSmallDiv.style.background = 'black'
                 createSmallDiv.dataset.type = `wall`
             } 
             else{
                 createSmallDiv.dataset.type = `way`
-                createSmallDiv.style.background = 'cyan'
+                createSmallDiv.style.background = 'rgb(11, 11, 231)'
             }
         }
     }
@@ -114,7 +115,36 @@ button.addEventListener('click', ()=>{
 })
 
 
+//    win()
+// duck.style.left = boxLeft + 'px'
+// duck.style.top = boxTop + 'px'
+// let boxTop = 270;
+// let boxLeft = 0;
+//let duck = document.getElementsByTagName('img')[0]
 
+// restart.addEventListener('click', ()=>{
+//     duck.style.left = boxLeft + 'px'
+//     duck.style.top = boxTop + 'px'
+// })
+// //CONDICAO DE VITORIA SE CHEGAR NO 240, 600
 
-
-
+// let win = () =>{
+//     if(boxTop === 240 && boxLeft === 600){
+//         alert('voce ganhou')
+//     }
+// }
+// document.addEventListener('keydown', (event) =>{
+//     let keyName = event.key
+//         if(keyName === 'ArrowUp' && boxTop > 0){
+//             boxTop -= 30
+//         }if(keyName === 'ArrowDown' && boxTop < 420){
+//             boxTop += 30
+//         }if(keyName === 'ArrowRight' && boxLeft < 600){
+//             boxLeft += 30
+//         }if(keyName === 'ArrowLeft' && boxLeft > 0){
+//             boxLeft -= 30
+//         }
+//     //    win()
+//     // duck.style.left = boxLeft + 'px'
+//     // duck.style.top = boxTop + 'px'
+// })
