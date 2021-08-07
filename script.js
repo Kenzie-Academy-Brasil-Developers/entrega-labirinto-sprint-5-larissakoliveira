955073108b186b02f29b79b290d8ef5b74acba45
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-08-06 21:04:11 -0300

    feat:add if da seta direita

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const body = document.getElementsByTagName('body')[0]
 const mainLabirinto = document.getElementById('labirinto')
+const button = document.getElementById('restart')
 
 let map = [
     "WWWWWWWWWWWWWWWWWWWWW",
@@ -19,6 +20,11 @@ let map = [
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
+
+let item;
+let createSmallDiv;
+let createBigDiv;
+
 let createLabyrinth = ()=>{
     
     for(let i = 0; i < map.length; i++){
@@ -27,20 +33,22 @@ let createLabyrinth = ()=>{
         mainLabirinto.appendChild(createBigDiv)
 
         for(let j = 0; j < map[i].length; j++){
-            let createSmallDiv = document.createElement('div')
+            createSmallDiv = document.createElement('div')
+            createSmallDiv.dataset.lineColumn = `${i}-${j}`
             createSmallDiv.classList.add('smallDiv')
             createBigDiv.appendChild(createSmallDiv)
-
+            
             if(map[i][j] === "S"){
-                let item = document.createElement('img')
+                item = document.createElement('img')
                 item.src = 'patin'
                 createSmallDiv.appendChild(item)
             }
             if(map[i][j] === "W"){
-                createSmallDiv.style.backgroundColor = 'blue'
+                createSmallDiv.style.background = 'blue'
+                createSmallDiv.dataset.type = `wall`
             } 
             else{
-                createSmallDiv.style.backgroundColor = 'white'
+                createSmallDiv.dataset.type = `way`
             }
         }
     }
@@ -50,24 +58,108 @@ let createLabyrinth = ()=>{
 
 createLabyrinth()
 
-let boxTop = 270;
-let boxLeft = 0;
+                //primeiro pega o proximo elemento no html pelo queryselector
+               //verificar se ele é parede
+               //ses nao for, fazer o append, se for nao fazer nada
 
-document.addEventListener('keydown', (event) =>{
-    let duck = document.getElementsByTagName('img')[0]
-    let keyName = event.key
+document.addEventListener('keydown', (event) => {
+        let ducksPosition = item.parentElement.dataset.lineColumn
+        ducksPosition = ducksPosition.split('-')
+        console.log(ducksPosition + 'ducksposition')
+        let keyName = event.key
+        let linha = ducksPosition[0]
+        console.log(linha + 'linha')
+        let coluna = ducksPosition[1]
+        console.log(coluna + 'coluna')
 
-        if(keyName === 'ArrowUp'){
-            boxTop -= 30
-        }if(keyName === 'ArrowDown'){
-            boxTop += 30
-        }if(keyName === 'ArrowRight'){
-            boxLeft += 30
-        }if(keyName === 'ArrowLeft'){
-            boxLeft -= 30
-        }
+            if(keyName === 'ArrowUp'){
+            //     coluna--
+            //     console.log(coluna)
+            //     let proximap = document.querySelector(`[data-line-column = "${linha}-${coluna}"]`)
+            //   // proximap.value === 'W'
 
-    duck.style.left = boxLeft + 'px'
-    duck.style.top = boxTop + 'px'
+            //     console.log(proximap.dataset.type)
+   
+              createSmallDiv.appendChild(item)
+            }if(keyName === 'ArrowDown'){
+
+            }if(keyName === 'ArrowRight'){
+                coluna++
+                let proximap = document.querySelector(`[data-line-column = "${linha}-${coluna}"]`)
+                if(proximap.dataset.type === 'way'){
+                    console.log(item)
+                    proximap.appendChild(item)
+                    console.log(proximap)
+                }
+                console.log(proximap.value)
+            }if(keyName === 'ArrowLeft'){
+                
+            }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   //    win()
+    // duck.style.left = boxLeft + 'px'
+    // duck.style.top = boxTop + 'px'
+
+
+// let boxTop = 270;
+// let boxLeft = 0;
+//let duck = document.getElementsByTagName('img')[0]
+
+// restart.addEventListener('click', ()=>{
+//     duck.style.left = boxLeft + 'px'
+//     duck.style.top = boxTop + 'px'
+// })
+
+
+// let updateMap = () =>{
+//     for(let i = 0; i < map.lengtsmallDivh; i++){
+//         for(let j = 0; j < map[i].length; j++){
+//             map.push()
+//         }
+//     }
+// }
+
+
+
+// //CONDICAO DE VITORIA SE CHEGAR NO 240, 600
+
+// let win = () =>{
+//     if(boxTop === 240 && boxLeft === 600){
+//         alert('voce ganhou')
+//     }
+// }
+
+
+
+// document.addEventListener('keydown', (event) =>{
+//     let keyName = event.key
+
+//         if(keyName === 'ArrowUp' && boxTop > 0){
+//             boxTop -= 30
+//         }if(keyName === 'ArrowDown' && boxTop < 420){
+//             boxTop += 30
+//         }if(keyName === 'ArrowRight' && boxLeft < 600){
+//             boxLeft += 30
+//         }if(keyName === 'ArrowLeft' && boxLeft > 0){
+//             boxLeft -= 30
+//         }
+//     //    win()
+//     // duck.style.left = boxLeft + 'px'
+//     // duck.style.top = boxTop + 'px'
+// })
+
 
